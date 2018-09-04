2aaf649cc86e6ed105e80290fa082258d0f150df
ydvorzhetskiy <dvorzhetskij@mail.ru>
2018-09-04 22:54:40 +0600

    Final

---
+++ src/main/java/ru/otus/spring/security/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package ru.otus.spring.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                // По умолчанию SecurityContext хранится в сессии
+                // Это необходимо, чтобы он нигде не хранился
+                // и данные приходили каждый раз с запросом
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers("/public").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/authenticated", "/success").authenticated()
+                .and()
+                // Включает Form-based аутентификацию
+                //.formLogin()
+                // Включает HTTP-basic
+                .httpBasic()
+                .and()
+                // Включает анонимную аутнетифкацию
+                // Здесь небольшая ошибка
+                .anonymous().principal("anonymous")
+                .and()
+                // Включает Remember-me аутентифкацию
+                .rememberMe().key("someSecret")
+        ;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("password").roles("ADMIN");
+    }
+}
